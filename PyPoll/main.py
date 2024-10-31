69411d8199f7176f565f214622277f1a0835180c
brenprie <brenprie@gmail.com>
2024-10-31 14:27:41 -0400

    Clean up commenting

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,17 +6,14 @@ import os
 file_to_load = os.path.join("Resources", "election_data.csv")  # Input file path
 file_to_output = os.path.join("Analysis", "election_analysis.txt")  # Output file path
 
-# INITIALIZE VARIABLES
+# Initialize variables
 numVotes = 0  # Total number of votes cast
 percentVotes = 0 # Percentage of votes (for a given candidate)
 
-# DEFINE LISTS AND DICTIONARIES TO TRACK CANDIDATE NAMES AND VOTE COUNTS
+# Define lists and dictionaries
 candidates = []  # list to hold the different candidates; candidates are in index 2 from data table 
 candidateVoteCounts = {} # dictionary to hold vote counts for each candidate
 candidateVotePercentages = {} # dictionary to hold vote percentages for each candidate
-
-# Winning Candidate and Winning Count Tracker
-
 
 # Open the CSV file and process it
 with open(file_to_load, "r", encoding="utf-8") as election_data:
@@ -43,30 +40,30 @@ with open(file_to_load, "r", encoding="utf-8") as election_data:
             # if the candidate is already in the candidate list, then add to its count in the dictionary
             candidateVoteCounts[candidate] += 1
 
-# GENERATE OUTPUT SUMMARY
-# Xpert Learning Assistant provided guidance re how to report each candidate/vote count on separate line, correct syntax to report percentage of votes for each candidate
-# Add to "output" title and total number of votes
-output = (
-    f"\nElection Results\n"
-    f"\n--------------------------------------\n"
-    f"\nTotal Votes: {numVotes}\n"
-    f"\n--------------------------------------\n"
-)
-# Loop through candidates to determine vote percentages, and for each candidate add percentage of votes and vote count to "output"  
-for candidate in candidateVoteCounts:
-    # Calculate percentage of votes for each candidate, add to "output"
-    percentVotes = (candidateVoteCounts[candidate] / numVotes) * 100
-    output += f"\n{candidate}: {percentVotes:.3f}% ({candidateVoteCounts[candidate]})\n"
-    # Update the winning candidate if this one has more votes
-
-# Determine winner (I used max function on dictionary), and add winner to "output"
-# Xpert Learning Assistant provided guidance re syntax to use max on dictionary    
-winner = max(candidateVoteCounts, key=candidateVoteCounts.get)
-output += (
-    f"\n--------------------------------------\n"
-    f"\nWinner: {winner}\n"
-    f"\n--------------------------------------\n"
+# Generate output summary
+    
+    # Add to "output" title and total number of votes
+    output = (
+        f"\nElection Results\n"
+        f"\n--------------------------------------\n"
+        f"\nTotal Votes: {numVotes}\n"
+        f"\n--------------------------------------\n"
     )
+
+    # Loop through candidates to determine vote percentages, and for each candidate add percentage of votes and vote count to "output"  
+    for candidate in candidateVoteCounts:
+        # Calculate percentage of votes for each candidate, add to "output"
+        percentVotes = (candidateVoteCounts[candidate] / numVotes) * 100
+        output += f"\n{candidate}: {percentVotes:.3f}% ({candidateVoteCounts[candidate]})\n"
+        # Update the winning candidate if this one has more votes
+
+    # Determine winner (I used max function on dictionary), and add winner to "output"
+    winner = max(candidateVoteCounts, key=candidateVoteCounts.get)
+    output += (
+        f"\n--------------------------------------\n"
+        f"\nWinner: {winner}\n"
+        f"\n--------------------------------------\n"
+        )
 
 # Print output to terminal
 print(output)
