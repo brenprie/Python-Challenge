77c8dfdd005bdcf353ec799c6fcde759b4e556f3
brenprie <brenprie@gmail.com>
2024-11-02 22:33:08 -0400

    Refined code
    Two primary changes:
    1) Move percentage votes and winner calculations above the section on generating the "output" variable
    2) While my preferred method to find winner is to use max function (1 line!), for education purposes I add to comments a loop method that achieves same end

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,75 +2,96 @@
 import csv
 import os
 
-# Files to load and output (update with correct file paths)
+# Files to load and output
 file_to_load = os.path.join("Resources", "election_data.csv")  # Input file path
 file_to_output = os.path.join("Analysis", "election_analysis.txt")  # Output file path
 
 # Initialize variables
-numVotes = 0  # Total number of votes cast
-percentVotes = 0 # Percentage of votes (for a given candidate)
+totalVotes = 0  # total number of votes cast across all candidates
+voteOutput = "" # variable that holds candidates, their percentage of votes, and their vote counts
+winningCount = 0 # variable to hold winning vote count
 
 # Define lists and dictionaries
-candidates = []  # list to hold the different candidates; candidates are in index 2 from data table 
-candidateVoteCounts = {} # dictionary to hold vote counts for each candidate
-candidateVotePercentages = {} # dictionary to hold vote percentages for each candidate
+candidates = []  # list to hold the different candidates; candidates are index 2
+candidateVotes = {} # dictionary to hold vote counts for each candidate
 
-# Open the CSV file and process it
-with open(file_to_load, "r", encoding="utf-8") as election_data:
-    csvreader = csv.reader(election_data)
+# Read and process the CSV file
+with open(file_to_load, "r", encoding="utf-8") as electionData:
+    # Create csv reader
+    csvreader = csv.reader(electionData)
 
-    # Skip the header row
+    # skip the header row, so can proceed to dataset
     header = next(csvreader)
 
     # Loop through each row of the dataset and process it
     for row in csvreader:
 
         # Add to the running total number of votes
-        numVotes += 1
+        totalVotes += 1
 
         # Get the candidate's name from the row
+        # this could be skipped, with "row[2]" replacing "candidate" below, but this step increases logical transparency
         candidate = row[2] 
 
-        # If the candidate is not already in the candidate list, add them to the list
+        # If the candidate is not already in the candidate list:
         if candidate not in candidates:
+            # add candidate to list of candidates
             candidates.append(candidate)
-            # add key-value pair for candidate and start its vote count
-            candidateVoteCounts[candidate] = 1 # once candidate appears, add to their vote count 
+            # add candidate (key) and vote (value) to dictionary; start candidate's vote count here
+            candidateVotes[candidate] = 1  
+        # if the candidate is already in the candidate list: 
         else:
-            # if the candidate is already in the candidate list, then add to its count in the dictionary
-            candidateVoteCounts[candidate] += 1
+            # add to candidate's count in the dictionary
+            candidateVotes[candidate] += 1
 
-# Generate output summary
+    # Loop through candidates. For each candidate, determine vote percentage; output vote percentage and vote count to output variable  
+    for candidate in candidateVotes:
+        # Calculate candidate's percentage of total votes
+        PercentVote = (candidateVotes[candidate] / totalVotes) * 100
+        # Add candidate's percentage of votes, as well as vote count in parens, to vote output variable
+        voteOutput += f"\n{candidate}: {PercentVote:.3f}% ({candidateVotes[candidate]})\n"
+
+        # Compare vote count to winning count to determine winner
+        # NOTE: I include the loop method here for education purposes, but I employ a more succint method to find the winner below
+        # if candidateVotes[candidate] > winningCount:
+            # update winning vote count
+        #   winningCount = candidateVotes[candidate]
+            # update winning candidate
+        #   winner = candidate
+
+    # Determine winner - my preferred method: using max function on dictionary
+    winner = max(candidateVotes, key=candidateVotes.get)
     
-    # Add to "output" title and total number of votes
+# Generate output variable to hold outputs; output variable will be a string
+# NOTE: the below could be provided in a single string without concatinations, but I prefer the long form in this instance to illustrate the technique
+    
+    # Add to "output": title
     output = (
         f"\nElection Results\n"
         f"\n--------------------------------------\n"
-        f"\nTotal Votes: {numVotes}\n"
-        f"\n--------------------------------------\n"
-    )
-
-    # Loop through candidates to determine vote percentages, and for each candidate add percentage of votes and vote count to "output"  
-    for candidate in candidateVoteCounts:
-        # Calculate percentage of votes for each candidate, add to "output"
-        percentVotes = (candidateVoteCounts[candidate] / numVotes) * 100
-        output += f"\n{candidate}: {percentVotes:.3f}% ({candidateVoteCounts[candidate]})\n"
-        # Update the winning candidate if this one has more votes
-
-    # Determine winner (I used max function on dictionary), and add winner to "output"
-    winner = max(candidateVoteCounts, key=candidateVoteCounts.get)
+        )
+    # Add to "output": total number of votes
     output += (
+        f"\nTotal Votes: {totalVotes}\n" # could use {totalVotes:,} to introduce commas in long numbers
         f"\n--------------------------------------\n"
+        )
+    # Add to "output": vote output variable
+    output += (
+         f"{voteOutput}"
+         f"\n--------------------------------------\n"
+        )
+    # Add to "output": winner
+    output += (
         f"\nWinner: {winner}\n"
         f"\n--------------------------------------\n"
         )
 
-# Print output to terminal
+# Display output to terminal
 print(output)
 
-# Export output to a text file
-with open(file_to_output, "w") as txt_file:
-    txt_file.write(output)
+# Export output to text file
+with open(file_to_output, "w") as textFile:
+    textFile.write(output)
 
 
 
